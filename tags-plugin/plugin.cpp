28c974d0524489a6ac7dff2f121c6804cb50c585
Lt-Henry <quiqueiii@gmail.com>
2026-04-08 09:54:50 +0200

    added tags plugin from jammy branch (not yet integrated into sddm)

---
+++ tags-plugin/plugin.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2026 Lliurex project
+ *
+ * Author:
+ *  Enrique Medina Gremaldos <quique@necos.es>
+ *
+ * Source:
+ *  https://github.com/lliurex/lliurex-sddm-theme
+ *
+ * This file is a part of lliurex sddm theme.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#include "plugin.h"
+
+#include <QQmlExtensionPlugin>
+#include <QObject>
+#include <QQmlEngine>
+#include <QMimeData>
+#include <QAbstractItemModel>
+#include <QProcess>
+#include <QLocale>
+#include <QDir>
+
+#include <iostream>
+#include <fstream>
+#include <chrono>
+
+using namespace std;
+
+QStringList list_files(QString path)
+{
+
+    QDir dir(path);
+
+    dir.setFilter(QDir::Files);
+
+    return dir.entryList();
+
+}
+
+Tags::Tags(QObject* parent): QObject(parent)
+{
+    reload();
+}
+
+void Tags::reload()
+{
+
+    m_tagsModel.clear();
+    m_tagsModel = list_files(QString::fromLatin1("/etc/lliurex-auto-upgrade/tags/"));
+
+    m_autoTagsModel.clear();
+    m_autoTagsModel = list_files(QString::fromLatin1("/run/lliurex-auto-upgrade/tags/"));
+
+    m_systemTagsModel.clear();
+    m_systemTagsModel = list_files(QString::fromLatin1("/usr/share/lliurex-auto-upgrade/tags/"));
+
+    emit tagsChanged();
+}
+
+TagsPlugin::TagsPlugin(QObject* parent) : QQmlExtensionPlugin(parent)
+{
+}
+
+void TagsPlugin::registerTypes(const char* uri)
+{
+    qmlRegisterType<Tags> (uri, 1, 0, "Tags");
+    qmlRegisterAnonymousType<QMimeData>(uri, 1);
+    
+}
---
+++ tags-plugin/plugin.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2026 Lliurex project
+ *
+ * Author:
+ *  Enrique Medina Gremaldos <quique@necos.es>
+ *
+ * Source:
+ *  https://github.com/lliurex/lliurex-sddm-theme
+ *
+ * This file is a part of lliurex sddm theme.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#ifndef QML_LLIUREX_TAGS_PLUGIN
+#define QML_LLIUREX_TAGS_PLUGIN
+
+#include <QQmlExtensionPlugin>
+#include <QObject>
+#include <QList>
+#include <QMap>
+#include <QStringList>
+
+class Tags: public QObject
+{
+    Q_OBJECT
+    
+
+    Q_PROPERTY(QStringList tagsModel MEMBER m_tagsModel NOTIFY tagsChanged)
+    Q_PROPERTY(QStringList autoTagsModel MEMBER m_autoTagsModel NOTIFY tagsChanged)
+    Q_PROPERTY(QStringList systemTagsModel MEMBER m_systemTagsModel NOTIFY tagsChanged)
+
+    
+    public:
+    
+    explicit Tags(QObject* parent = nullptr);
+    
+    Q_INVOKABLE void reload();
+
+    Q_SIGNALS:
+
+    void tagsChanged();
+    
+    private:
+    QStringList m_tagsModel;
+    QStringList m_autoTagsModel;
+    QStringList m_systemTagsModel;
+
+};
+
+class TagsPlugin : public QQmlExtensionPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA (IID "Lliurex.Tags")
+
+    public:
+
+    explicit TagsPlugin(QObject *parent = nullptr);
+    void registerTypes(const char *uri) override;
+};
+
+
+#endif
